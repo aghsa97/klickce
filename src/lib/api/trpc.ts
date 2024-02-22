c0bc04817108bb0ce0a095f8291bfbd2e82b31dd
Mohammed Agha <83072153+aghsa97@users.noreply.github.com>
2024-02-22 10:33:16 +0100

    Revert "hot fix"
    This reverts commit 3ed5c9afc3bf3a04d2c478dea9c1bb3c66850233.

--- src/lib/api/trpc.ts
+++ src/lib/api/trpc.ts
@@ -52,12 +52,13 @@ export const router = t.router;
 export const publicProcedure = t.procedure;
 
 const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
-  // if (!ctx.auth?.userId) {
-  //   throw new TRPCError({
-  //     code: "UNAUTHORIZED",
-  //     message: "You have to be logged in to do this.",
-  //   });
-  // }
+  console.log("enforceUserIsAuthed", ctx.auth?.userId);
+  if (!ctx.auth?.userId) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "You have to be logged in to do this.",
+    });
+  }
   return next({
     ctx: {
       auth: {
