5eddaa9bc1c4ef7b8023eb8155d58bc6d21a1673
Mohammed Agha <83072153+aghsa97@users.noreply.github.com>
2024-02-26 12:45:45 +0100

    test - Home page no trpc

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -6,20 +6,14 @@ import Features from '@/components/landing-page/features';
 import FAQs from '@/components/landing-page/faqs';
 import OurMission from '@/components/landing-page/our-mission';
 import PricingPlan from '@/components/landing-page/pricing-plan';
-import Stats from '@/components/landing-page/stats';
-import { api } from '@/lib/trpc/api';
 
 export const revalidate = 900;
 
 async function Home() {
-    const mapsCount = await api.maps.getMapsCount.query();
-    const imagesCount = await api.images.getImagesCount.query();
-    const spotsCount = await api.spots.getSpotsCount.query();
 
     return (
         <main className="flex w-full flex-col items-center justify-center py-24 px-4 md:px-0 gap-10">
             <Hero />
-            <Stats mapsCount={mapsCount.count * 25} imagesCount={imagesCount.count * 250} spotsCount={spotsCount.count * 123} />
             <Features />
             <PricingPlan />
             <OurMission />
